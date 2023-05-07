7e5308b4a7b28271b5ad98aed015a6d6ea40ffb0
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-05-07 22:47:38 +0530

    Config updated

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
     db: {
-      host: "alibaba.mysql.polardb.ap-south-1.rds.aliyuncs.com",
-      user: "alibabatester",
-      password: "ggits12345678@",
-      database: "tester",
-      port:"3306"
-      // host: "localhost",
-      // user: "root",
-      // password: "",
+      // host: "alibaba.mysql.polardb.ap-south-1.rds.aliyuncs.com",
+      // user: "alibabatester",
+      // password: "ggits12345678@",
       // database: "tester",
+      // port:"3306"
+      host: "db4free.net",
+      user: "testerprx",
+      password: "12345678",
+      database: "tester",
     },
     listPerPage: 20,
   };
